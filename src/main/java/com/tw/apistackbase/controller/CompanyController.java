83a0fab73e94867fbdb8f393542934cac3e1418c
murphylee2048 <1134697131@qq.com>
2019-07-17 11:04:56 +0800

    [ML] feat: should_delete_company_by_id

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -4,9 +4,10 @@ import com.tw.apistackbase.model.Company;
 import com.tw.apistackbase.model.Employee;
 import com.tw.apistackbase.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,14 +25,15 @@ public class CompanyController {
         return companyService.findCompanyById(companyId);
     }
 
+    @PostMapping("/companies")
+    public ResponseEntity addCompany(@RequestBody Company company) {
+        companyService.addCompany(company);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
     @GetMapping("/companies/{id}/employees")
     public List<Employee> findAllEmployees(@PathVariable String id) {
         return companyService.findAllEmployees(id);
-    }
-
-    @PostMapping("/companies")
-    public void addCompany(@RequestBody Company company) {
-        companyService.addCompany(company);
     }
 
     @PutMapping("/company/{companyId}")
